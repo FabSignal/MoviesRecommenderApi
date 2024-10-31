f9b93e02b959097f3ff241853e35439fa240b754
FabSignal <fabianafernandezf8@gmail.com>
2024-10-31 01:41:33 -0300

    F 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import pandas as pd
 from datetime import datetime
 from fastapi.responses import RedirectResponse
@@ -33,14 +33,49 @@ meses = {
 # 1- Cantidad de filmaciones por mes
 @app.get("/cantidad_filmaciones_mes/{mes}")
 def cantidad_filmaciones_mes(mes: str):
-    # Convertir el mes ingresado a número
-    mes_num = meses.get(mes.lower())
+       # Convertir el mes ingresado a minúsculas
+    mes = mes.lower()
+    # Obtener el número del mes y verificar su validez
+    mes_num = meses.get(mes)
     
     if mes_num is None:
-        return {"error": "Mes no válido. Usa el nombre del mes en español."}
+        raise HTTPException(status_code=400, detail= 'Mes no válido. Usa el nombre completo del mes en español.') 
     
     # Filtrar las películas estrenadas en el mes específico
-    data['release_date'] = pd.to_datetime(data['release_date'], errors='coerce')
     cantidad = data[data['release_date'].dt.month == mes_num].shape[0]
     
-    return {"mensaje": f"{cantidad} cantidad de películas fueron estrenadas en el mes de {mes.capitalize()}"}
+    return {"mensaje": f"En {mes.capitalize()}, se estrenaron {cantidad} películas."}
+
+
+# 2.
+
+# Asegúrate de que la columna 'release_date' esté en formato de fecha
+data['release_date'] = pd.to_datetime(data['release_date'], errors='coerce')
+
+# Diccionario que incluye formas con y sin acento
+dias_semana = {
+    'lunes': 0,
+    'martes': 1,
+    'miércoles': 2, 'miercoles': 2,
+    'jueves': 3,
+    'viernes': 4,
+    'sábado': 5, 'sabado': 5,
+    'domingo': 6
+}
+
+@app.get("/cantidad_filmaciones_dia/{dia}")
+def cantidad_filmaciones_dia(dia: str):
+    # Convertir el día ingresado a minúsculas para uniformidad
+    dia = dia.lower()
+    
+    # Verificar si el día ingresado es válido en el diccionario
+    if dia not in dias_semana:
+        raise HTTPException(status_code=400, detail= 'Día no válido. Elige entre: lunes, martes, miércoles, jueves, viernes, sábado o domingo.')
+
+    # Filtrar las películas que fueron estrenadas en el día de la semana deseado
+    dia_num = dias_semana[dia]
+    peliculas_dia = data[data['release_date'].dt.weekday == dia_num]
+
+    # Contar la cantidad de películas y retornar el mensaje
+    cantidad = len(peliculas_dia)
+    return {"mensaje": f"{cantidad} cantidad de películas fueron estrenadas en días {dia.capitalize()}"}
